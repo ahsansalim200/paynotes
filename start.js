6df9b19fa84ea9af8c76a757623b25f3377bfe32
Ahsan SALIM <ahsan.salim@amadeus.com>
2019-03-02 17:20:57 +0530

    updating port number

--- start.js
+++ start.js
@@ -17,6 +17,6 @@ mongoose.connection
         console.log(`Connection error: ${err.message}`);
     });
 
-const server = app.listen(3001, () => {
+const server = app.listen(80, () => {
         console.log(`Express is running on port ${server.address().port}`);
 });
